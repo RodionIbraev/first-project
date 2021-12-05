b98e19b88c9b84e0cb489755358f42646c12a984
Rodion Ibraev <roriibra@gmail.com>
2021-12-05 11:10:28 +0300

    Добавленны файлы

---
+++ first_project/app/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from datetime import datetime
+import os
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now().strftime('%H:%M')
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    return HttpResponse(', '.join(os.listdir()))
